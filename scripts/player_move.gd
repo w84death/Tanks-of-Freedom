35b110303ad5c94fa81e260d63572f9f92ebde76
jasam <jasam@KaoS.wrmz>
2015-01-23 21:04:40 +0100

    position calculator

--- scripts/player_move.gd
+++ scripts/player_move.gd
@@ -3,6 +3,8 @@ extends Sprite
 
 var position = Vector2(16,154)
 var move_vector = Vector2(32,16)
+var Position = preload("position.gd")
+
 
 func _input(event):
 	if (Input.is_action_pressed('player1_up')):
@@ -16,6 +18,9 @@ func _input(event):
 		
 	if (Input.is_action_pressed('player1_right')):
 		position += Vector2(move_vector.x/2,move_vector.y/2)
+		
+	if (Input.is_action_pressed('player1_action1')):
+		Position.abstract_position(position, move_vector)
 		
 	self.set_pos(position)
 	
