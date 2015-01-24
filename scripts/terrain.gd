cbd3facb39b7e9b82ee1a21f2b8250f7fcc31f74
CzlowiekImadlo <quak@tlen.pl>
2015-01-24 17:35:27 +0100

    Extended terrain objects

---
+++ scripts/terrain.gd
@@ -0,0 +1,20 @@
+
+extends AnimatedSprite
+export var position_on_map = Vector2(0,0)
+var current_map
+
+var type
+
+func get_pos_map():
+	return position_on_map
+	
+func set_pos_map(new_position):
+	self.set_pos(current_map.map_to_world(new_position))
+	position_on_map = new_position
+
+func _ready():
+	add_to_group("terrain")
+	current_map = get_node("/root/game/pixel_scale/map")
+	pass
+
+
