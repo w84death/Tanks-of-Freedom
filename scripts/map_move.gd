9024e08a0b1da285504c476ab2c88b19962e2e7c
Krzysztof Jankowski <kjankowski@ioki.com.pl>
2015-01-27 22:36:34 +0100

    simple move_to tween for terrain

--- scripts/map_move.gd
+++ scripts/map_move.gd
@@ -3,6 +3,11 @@ extends TileMap
 var mouse_dragging = false
 var pos = self.get_pos()
 
+var sX = 0
+var sY = 0
+var k = 0.98
+var target = Vector2(-200,-150)
+
 func _input(event):
 	if(event.type == InputEvent.MOUSE_BUTTON):
 		if (event.button_index == BUTTON_LEFT):
@@ -12,10 +17,21 @@ func _input(event):
 		if (mouse_dragging):
 			pos.x = pos.x + event.relative_x / 2
 			pos.y = pos.y + event.relative_y / 2
+			target = pos
 			self.set_pos(pos)
+
+func _process(delta):
+	if not pos == target:
+		self.sX = self.k * self.sX + (1.0 - self.k) * target.x;
+		self.sY = self.k * self.sY + (1.0 - self.k) * target.y;
+		self.set_pos(Vector2(self.sX,self.sY))
+
+func move_to(target):
+	self.target = target;
 
 func _ready():
 	set_process_input(true)
+	set_process(true)
 	pass
 
 
