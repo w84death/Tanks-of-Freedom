65d206329e238d42b8f47f3867093d7ef76bd4e6
Krzysztof Jankowski <kjankowski@ioki.com.pl>
2015-02-07 14:15:33 +0100

    gui button

--- scripts/game_logic.gd
+++ scripts/game_logic.gd
@@ -59,8 +59,9 @@ func load_map(map_template):
 	
 	scale_root.add_child(current_map)
 	self.add_child(hud)
-	menu.raise()
-	cursor.raise()
+	self.move_child(hud,0)
+	self.move_child(scale_root, 0)
+	self.move_child(get_node("corners"), 0)
 	
 	game_scale = scale_root.get_scale()
 	action_controller = preload("action_controller.gd").new()
@@ -102,15 +103,12 @@ func toggle_menu():
 func load_menu():
 	is_intro = false
 	self.add_child(menu)
-	cursor.show()
-	cursor.raise()
+	self.add_child(cursor)
 	self.remove_child(intro)
 	intro.queue_free()
 	
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	menu.init_root(self)
-	cursor.hide()
-	self.add_child(cursor)
 	self.add_child(intro)
 	pass
