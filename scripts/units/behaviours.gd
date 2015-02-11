fe6f36030bc619797452912e1526afc9186c3c3a
jasam <jasam@KaoS.wrmz>
2015-02-11 19:10:46 +0100

    AI - a bit better capture scoring

--- scripts/units/behaviours.gd
+++ scripts/units/behaviours.gd
@@ -7,9 +7,18 @@ const action_capture = 2
 var ap_cost_modifier = 4
 var path_size_modifier = 8
 var action_type_modifiers = [3, 2, 20]
+var capture_modifiers = [3,2,2]
+
+const ACTION_ATTACK = 0
+const ACTION_MOVE   = 1
+const ACTION_CAPTURE = 2
+const ACTION_SPAWN = 3
 
 func estimate_action(action_type, path_size, ap_cost):
 	var score = 100
+	if action_type == ACTION_CAPTURE:
+		score = 50 * capture_modifiers[type]
+
 	score = score * action_type_modifiers[action_type]
 	score = score - ap_cost_modifier * ap_cost
 	score = score - path_size_modifier * path_size
