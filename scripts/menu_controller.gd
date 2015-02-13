709de33b75905c5b96333e203496a7119a25e0d4
CzlowiekImadlo <quak@tlen.pl>
2015-02-13 19:40:31 +0100

    Connected menus

--- scripts/menu_controller.gd
+++ scripts/menu_controller.gd
@@ -1,11 +1,17 @@
 
 var root
+var control_node
 
-var map_0_button
-var map_1_button
-var map_2_button
-var map_3_button
+var cpu_vs_cpu_button
+var human_vs_cpu_button
+var human_vs_human_button
 var close_button
+
+var maps_sub_menu = preload("res://gui/menu_maps.xscn").instance()
+var maps_tutorial_button
+var maps_forest_button
+var maps_city_button
+var maps_close_button
 
 var sound_toggle_button
 var music_toggle_button
@@ -16,10 +22,11 @@ var music_toggle_label
 var shake_toggle_label
 
 func _ready():
-	map_0_button = get_node("control/game_controls/map_0")
-	map_1_button = get_node("control/game_controls/map_1")
-	map_2_button = get_node("control/game_controls/map_2")
-	map_3_button = get_node("control/game_controls/map_3")
+	control_node = get_node("control")
+	
+	cpu_vs_cpu_button = get_node("control/game_controls/cpu_cpu")
+	human_vs_cpu_button = get_node("control/game_controls/1p_cpu")
+	human_vs_human_button = get_node("control/game_controls/1p_2p")
 	close_button = get_node("control/game_controls/close")
 
 	sound_toggle_button = get_node("control/settings_controls/sound_toggle")
@@ -30,24 +37,40 @@ func _ready():
 	music_toggle_label = music_toggle_button.get_node("Label")
 	shake_toggle_label = shake_toggle_button.get_node("Label")
 	
-	map_0_button.connect("pressed", self, "load_small_map")
-	map_1_button.connect("pressed", self, "load_large_map")
+	human_vs_cpu_button.connect("pressed", self, "show_maps_menu")
+	human_vs_human_button.connect("pressed", self, "show_maps_menu")
+	
 	sound_toggle_button.connect("pressed", self, "toggle_sound")
 	music_toggle_button.connect("pressed", self, "toggle_music")
 	shake_toggle_button.connect("pressed", self, "toggle_shake")
 	
 	close_button.connect("pressed", root, "toggle_menu")
 	self.refresh_buttons_labels()
+	self.load_maps_menu()
+	
+func load_maps_menu():
+	maps_sub_menu.hide()
+	self.add_child(maps_sub_menu)
+	
+	maps_tutorial_button = maps_sub_menu.get_node("control/menu_controls/tutorial")
+	maps_forest_button = maps_sub_menu.get_node("control/menu_controls/forest")
+	maps_city_button = maps_sub_menu.get_node("control/menu_controls/city")
+	maps_close_button = maps_sub_menu.get_node("control/menu_controls/close")
+	
+	maps_close_button.connect("pressed", self, "hide_maps_menu")
+	
+func show_maps_menu():
+	control_node.hide()
+	maps_sub_menu.show()
+	
+func hide_maps_menu():
+	control_node.show()
+	maps_sub_menu.hide()
 
 func load_map(name):
 	root.load_map(name)
 	root.toggle_menu()
-
-func load_small_map():
-	self.load_map('crossing')
-
-func load_large_map():
-	self.load_map('forest')
+	self.hide_maps_menu()
 
 func toggle_sound():
 	root.sound_settings['sound_enabled'] = not root.sound_settings['sound_enabled']
