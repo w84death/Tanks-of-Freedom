7c4cc71fe317cd9fe6fc2614f92ed665f098661e
Krzysztof Jankowski <kjankowski@ioki.com.pl>
2015-03-22 18:08:46 +0100

    destructed tiles

---
+++ eyecandy/destroyed_tile.gd
@@ -0,0 +1,22 @@
+
+extends Sprite
+
+export var damaged = false
+var max_frames
+
+func check_destruction():
+	if damaged:
+		self.set_frame(randi() % max_frames)
+	else:
+		self.set_frame(0)
+
+func set_damage():
+	damaged = true
+
+func _ready():
+	randomize()
+	max_frames = self.get_vframes() * self.get_hframes()
+	check_destruction()
+	pass
+
+
