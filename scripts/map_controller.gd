7ed2cf7c75f750a349495d6f5340fdeac432f30b
Krzysztof Jankowski <kjankowski@ioki.com.pl>
2015-04-27 19:40:02 +0200

    fog of war for single player

--- scripts/map_controller.gd
+++ scripts/map_controller.gd
@@ -259,20 +259,20 @@ func clear_fog():
 	for unit in units:
 		# cpu vs cpu mode
 		# show everything aka spectator mode
-		if root.settings['cpu_0'] and root.settings['cpu_1']:
+		if root.settings['cpu_0'] && root.settings['cpu_1']:
 			self.clear_fog_range(unit.position_on_map,2)
 		else:
-			if unit.player == current_player and (( unit.player == 0 and not root.settings['cpu_0'] ) or (unit.player == 1 and not root.settings['cpu_1'])):
+			if ( unit.player == 0 and not root.settings['cpu_0'] ) or (unit.player == 1 and not root.settings['cpu_1']):
 				self.clear_fog_range(unit.position_on_map,2)
 				
 	for building in buildings:
 		# cpu vs cpu mode
 		# show everything aka visitor mode
-		if root.settings['cpu_0'] and root.settings['cpu_1']:
+		if root.settings['cpu_0'] && root.settings['cpu_1']:
 			self.clear_fog_range(building.position_on_map,3)
 		else:
 			if building.player > -1: # ocupy
-				if building.player == current_player and (( building.player == 0 and not root.settings['cpu_0'] ) or (building.player == 1 and not root.settings['cpu_1'])):
+				if ( building.player == 0 and not root.settings['cpu_0'] ) or (building.player == 1 and not root.settings['cpu_1']):
 					self.clear_fog_range(building.position_on_map,3)
 	return
 
