7d69a7d9d1198104490ae057227d099a08ea9f48
WermooZ <p.weremczuk@gmail.com>
2015-04-27 20:31:07 +0200

    some cleaning up

---
+++ scripts/ai/pathfinding/cost_grid.gd
@@ -0,0 +1,36 @@
+var abstract_map
+var grids = []
+var tileObject = preload('tile_object.gd')
+
+#rearrange
+func _init(abstract_map_new):
+	abstract_map = abstract_map_new
+
+func prepare_cost_maps(own_buildings, own_units, terrain):
+	grids.clear()
+	for unit_type in range(0,3):
+		var cost_map = self.__fillCostMap(abstract_map.tiles_cost_map[unit_type], own_units, own_buildings, terrain)
+		grids.insert(unit_type, self.__prepareGrid(cost_map))
+	return grids
+
+func __fillCostMap(cost_map, units, ownBuildings, terrain):
+	for pos in units:
+		var unit_pos = units[pos].get_pos_map()
+		cost_map[unit_pos.x][unit_pos.y] = tileObject.NON_WALKABLE
+
+	for pos in ownBuildings:
+		var unit_pos = ownBuildings[pos].get_pos_map()
+		cost_map[unit_pos.x][unit_pos.y] = tileObject.NON_WALKABLE
+
+	for pos in terrain:
+		var unit_pos = terrain[pos].get_pos_map()
+		cost_map[unit_pos.x][unit_pos.y] = tileObject.NON_WALKABLE
+
+	return cost_map
+
+func __prepareGrid(cost_map):
+	var grid = {}
+	for x in range(cost_map.size()):
+		for y in range(cost_map[x].size()):
+			grid[Vector2(x,y)] = tileObject.new(cost_map[x][y])
+	return grid
---
+++ scripts/nearby_tiles_calculator.gd
@@ -0,0 +1,17 @@
+const MAX_RANGE = 10
+
+func get_calculated_nearby_tiles():
+	for distance in range(1, MAX_RANGE):
+
+		var max_distance = distance *2 -1
+		var tiles = []
+
+		for x in range(-distance, distance + 1):
+			for y in range(-distance, distance  + 1):
+				# we are skipping current tile
+				if (!(x == 0 && y == 0) && !(abs(x) + abs(y) > max_distance)):
+					tiles.append(Vector2(x, y))
+
+		print(tiles)
+
+	return [[[]]]
