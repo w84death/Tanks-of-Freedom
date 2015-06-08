6572bb139209500de6468ee234a310ed4f9d7bed
CzlowiekImadlo <quak@tlen.pl>
2015-06-08 17:42:36 +0200

    Added Base Defence and Airport Assault Part 1 to campaign

--- maps/campaign.gd
+++ maps/campaign.gd
@@ -6,6 +6,8 @@ var maps = [
     {'label': 'Base Assault', 'player': 1, 'file': preload("res://maps/campaign/base_assault.gd").new()},
     {'label': 'Peninsula', 'player': 1, 'file': preload("res://maps/campaign/peninsula.gd").new()},
     {'label': 'Recapture', 'player': 0, 'file': preload("res://maps/campaign/administration_recapture.gd").new()},
+    {'label': 'Base Defence', 'player': 0, 'file': preload("res://maps/campaign/base_defence.gd").new()},
+    {'label': 'Airport Pt.1', 'player': 1, 'file': preload("res://maps/campaign/airport_part_1.gd").new()},
 ]
 
 func get_map_data(map_name):
