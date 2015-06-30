78b018acbf44b7ca3bd8eb3d846eda85e51a5328
CzlowiekImadlo <quak@tlen.pl>
2015-06-30 22:54:29 +0200

    Added prev/next buttons to campaign menu

--- scripts/controllers/campaign_menu_controller.gd
+++ scripts/controllers/campaign_menu_controller.gd
@@ -6,6 +6,8 @@ var current_campaign_map = 0
 
 var back_button
 var start_button
+var prev_button
+var next_button
 
 var mission_num
 var mission_name
@@ -17,10 +19,13 @@ func init_root(root_node):
     self.bind_campaign_menu()
     self.attach_campaign_menu()
     self.fill_mission_data(self.current_campaign_map)
+    self.manage_switch_buttons()
 
 func bind_campaign_menu():
     self.back_button = self.campaign_menu.get_node("control/dialog_controls/close")
     self.start_button = self.campaign_menu.get_node("control/dialog_controls/start_button")
+    self.prev_button = self.campaign_menu.get_node("control/dialog_controls/prev_button")
+    self.next_button = self.campaign_menu.get_node("control/dialog_controls/next_button")
 
     self.mission_num = self.campaign_menu.get_node("control/dialog_controls/title/mission_num")
     self.mission_name = self.campaign_menu.get_node("control/dialog_controls/title/mission_name")
@@ -29,6 +34,8 @@ func bind_campaign_menu():
 
     self.back_button.connect("pressed", self, "hide_campaign_menu")
     self.start_button.connect("pressed", self, "start_mission")
+    self.prev_button.connect("pressed", self, "switch_to_prev")
+    self.next_button.connect("pressed", self, "switch_to_next")
 
 func attach_campaign_menu():
     self.root.dependency_container.controllers.menu_controller.add_child(self.campaign_menu)
@@ -46,6 +53,7 @@ func fill_mission_data(mission_num):
     self.set_mission_name(self.root.dependency_container.campaign.get_map_name(mission_num))
     self.set_mission_description(self.root.dependency_container.campaign.get_map_description(mission_num))
     self.set_team(self.root.dependency_container.campaign.get_map_player(mission_num))
+    self.manage_switch_buttons()
 
 func set_mission_num(number):
     self.mission_num.set_text(str(number))
@@ -66,3 +74,23 @@ func start_mission():
     self.root.load_map(self.current_campaign_map, false)
     self.hide_campaign_menu()
     self.root.toggle_menu()
+
+func switch_to_prev():
+    if self.current_campaign_map > 0:
+        self.current_campaign_map = self.current_campaign_map - 1
+        self.fill_mission_data(self.current_campaign_map)
+
+func switch_to_next():
+    if self.current_campaign_map < (self.root.dependency_container.campaign.maps.size() - 1):
+        self.current_campaign_map = self.current_campaign_map + 1
+        self.fill_mission_data(self.current_campaign_map)
+
+func manage_switch_buttons():
+    if self.current_campaign_map == 0:
+        self.prev_button.hide()
+    else:
+        self.prev_button.show()
+    if (self.current_campaign_map + 1) == self.root.dependency_container.campaign.maps.size():
+        self.next_button.hide()
+    else:
+        self.next_button.show()
