d5282a1b03b4726185e29afbabb0179ab4d50e17
Wojciech Chojnacki <quak@tlen.pl>
2015-07-20 17:14:23 +0200

    Added default camera zoom to engine settings

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -7,6 +7,7 @@ var camera_zoom_range = [1,6]
 func init_root(root_node):
 	self.root = root_node
 	self.camera = root_node.scale_root
+	self.set_camera_zoom(Globals.get("tof/default_zoom"))
 
 func camera_zoom_in():
 	var scale = self.camera.get_scale()
@@ -23,3 +24,6 @@ func camera_zoom_out():
 	if self.abstract_map.map != null:
 		self.abstract_map.map.scale = self.camera.get_scale()
 	self.root.dependency_container.controllers.menu_controller.update_zoom_label()
+
+func set_camera_zoom(zoom_value):
+	self.camera.set_scale(Vector2(zoom_value, zoom_value))
