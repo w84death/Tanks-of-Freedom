059cc2950d5a989feac250e799461a250defbe7d
CzlowiekImadlo <quak@tlen.pl>
2015-09-17 19:51:11 +0200

    Updated campaing missions briefings

--- maps/campaign.gd
+++ maps/campaign.gd
@@ -7,13 +7,13 @@ var maps = [
         'label': 'MayDay Square',
         'player': 0,
         'file': preload("res://maps/campaign/maidan.gd").new(),
-        'description': 'AFTER DAYS OF PROTESTS, WE FINALLY SAY ENOUGH IS ENOUGH. CORRUPTION AND ABUSE HAS TO STOP! PROTESTERS OF MAYDAY SQUARE ARE READY TO FIGHT! TAKE COMMAND OF THE FREEDOM FIGHTERS AND ASSAULT PRESIDENTAL PALACE.'
+        'description': 'After days of protests, we finally say enough is enough. Corruption and abuse has to stop! Protesters of Mayday Square are ready to fight! Take command of the freedom fighters and assault Presidental Palace.'
     },
     {
         'label': 'Eastern city',
         'player': 0,
         'file': preload("res://maps/campaign/eastern_city.gd").new(),
-        'description': 'Even though the events of Mayday Square allowed for relatively peacefull change of government, there is still a lot of tension. We have reports of group of militiants loyal to the old president. Find and capture their center of operations before their actions cause civil war.'
+        'description': 'Even though the events of Mayday Square allowed for relatively peacefull change of government, there is still a lot of tension. We have reports of a group of militiants loyal to the old president. Find and capture their center of operations before their actions cause civil war.'
     },
     {
         'label': 'Riots',
@@ -31,43 +31,43 @@ var maps = [
         'label': 'Peninsula',
         'player': 1,
         'file': preload("res://maps/campaign/peninsula.gd").new(),
-        'description': 'Days of fighting are finnally paying off. Now we have support from our strong neighbouring country as well. We are ready to push the usurpers out of the peninsula. When we succeed, there will be no way for them to re-take it. This will be our great victory!'
+        'description': 'Days of fighting are finnally paying off. Now we have the support of our strong neighbouring country. We are ready to push the usurpers out of the peninsula. When we succeed, there will be no way for them to re-take it. This will be our great victory!'
     },
     {
         'label': 'Recapture',
         'player': 0,
         'file': preload("res://maps/campaign/administration_recapture.gd").new(),
-        'description': 'Situation is deteriorating quickly. We have to start pushing back, or we will loose all of our progress in this conflict. We are ready to liberate city where the rebels began their operations. Let us show them, that simple bandits are no match for our military.'
+        'description': 'Situation is deteriorating quickly. We have to start pushing back, or we will loose all of our progress in this conflict. We are ready to liberate the city, where the rebels began their operations. Let us show them, and the people, that simple bandits are no match for our military.'
     },
     {
         'label': 'Base Defence',
         'player': 0,
         'file': preload("res://maps/campaign/base_defence.gd").new(),
-        'description': 'Alert! Rebels are attacking one of our military bases in the eastern region! We must not allow them to capture more equipment. Defend the base and destroy rebel forces in the region.'
+        'description': 'Alert! Rebels are attacking one of our military bases in the eastern region! We must not allow them to capture any more of our assets. Defend the base and destroy the rebel forces in the region.'
     },
     {
         'label': 'Airport Pt.1',
         'player': 1,
         'file': preload("res://maps/campaign/airport_part_1.gd").new(),
-        'description': 'Fighting between our militia and government forces is not going as good as we would hope. We need to show them, that we are a force to be reckoned with. Nearby airport is not of huge strategic importance, but it will be our manifestation of power! Capture it!'
+        'description': 'Fighting between our militia and the government forces is not going as good as we would hope. We need to show the world, that we are not giving up just yet. Nearby airport is not of huge strategic importance, but capturing it will be our manifestation of power!'
     },
     {
         'label': 'Airplane',
         'player': 1,
         'file': preload("res://maps/campaign/airplane.gd").new(),
-        'description': 'We have intercepted a transmission about important military airplane, that will be flying over the area. We have identified perfect spot to launch missiles from, but it is guarded by the enemy. Muster your forces and get into position to shoot down the transport.'
+        'description': 'Thanks to the airport equipment, we have intercepted a transmission about an important military airplane, that will be flying over the area. We have identified the perfect spot to launch missiles from, but it is guarded by the enemy. Muster your forces and get into position to shoot down the transport.'
     },
     {
         'label': 'Field Command',
         'player': 0,
         'file': preload("res://maps/campaign/field_command.gd").new(),
-        'description': 'These separatists just shot down a civilian airplane, hundreds of people died. Their violence has to stop! Our scouts found the enemy field command center for this region. Our forces are small, but we should be able to gather more men and tanks in the nearby city.'
+        'description': 'These separatists just shot down a civilian airplane, hundreds of people died. Their violence has to stop! Our scouts have found the enemy field command center for this region. Our resources are limited, but we should be able to gather more men and tanks in the nearby city.'
     },
     {
         'label': 'Airport Pt.2',
         'player': 0,
         'file': preload("res://maps/campaign/airport_part_2.gd").new(),
-        'description': 'Rebel forces are in chaos after loosing their local HQ. Airport is still heavily guarded, but they will not recieve new reinforcements anytime soon. We managed to set up a small base. We have to attack before they can pick themselves up.'
+        'description': 'The rebel forces have scattered after loosing their local HQ. The Airport is still heavily guarded, but they will not recieve any new reinforcements anytime soon. Recon team managed to set up a small base. We have to attack before they can pick themselves up.'
     },
     {
         'label': 'Convoy',
@@ -79,13 +79,13 @@ var maps = [
         'label': 'Crash Site',
         'player': 1,
         'file': preload("res://maps/campaign/crash_site.gd").new(),
-        'description': 'Plane we attacked went down deep within the forest. We must not allow anyone to reach the crash site. Without the wreckage, no one will be able to proove who and how destroyed it. International investigators are on their way, we have to stop them.'
+        'description': 'The Plane we attacked went down deep within the forest. We must not allow anyone to reach the crash site. Without the wreckage, no one will be able to proove who and how destroyed it. International investigators are on their way, we have to stop them.'
     },
     {
         'label': 'Airport Pt.3',
         'player': 1,
         'file': preload("res://maps/campaign/airport_part_3.gd").new(),
-        'description': 'It has been months since we started fighting over the airport. It is hardly an airport anymore, everything reduced to rubble - the terminal, the tower, surrounding city. Even so, side that is able to control it will show others who is the strongest.'
+        'description': 'It has been months since we started fighting over this wretched airport. It is hardly an airport anymore either, with everything reduced to rubble - the terminal, the tower, surrounding city. Even so, it is still a symbol of who has the upper hand here, and it should be ours!'
     },
 ]
 
