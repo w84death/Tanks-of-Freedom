484c144739111fd72f56bd22a522e015ce089a03
Krzysztof Jankowski <kjankowski@ioki.com.pl>
2015-10-21 20:37:49 +0200

    message fix

--- scripts/message.gd
+++ scripts/message.gd
@@ -10,12 +10,7 @@ var active = false
 func show_message(title,message):
 	self.active = true
 	hud_title.set_text(title)
-	hud_message.clear()
-	for line in message:
-		hud_message.add_text(line)
-		hud_message.newline()
-		hud_message.newline()
-	#self.show()
+	hud_message.set_text(message)
 	self.raise()
 	return
 
