45e304d363d18f591cd214fc0fd3ad35000b1b4c
CzlowiekImadlo <quak@tlen.pl>
2016-01-14 18:00:41 +0100

    Fixed buildings not clearing fog in demo mode

--- scripts/fog_controller.gd
+++ scripts/fog_controller.gd
@@ -103,7 +103,7 @@ func __get_units_to_unhide(player):
 
 func __get_buildings_to_unhide(player):
     if root.settings['cpu_0'] && root.settings['cpu_1']:
-        return self.bag.positions.buildings_player_none
+        return self.bag.positions.all_buildings
     else:
         if self.is_cpu(player):
             return self.bag.positions.get_player_buildings((player + 1) % 2)
