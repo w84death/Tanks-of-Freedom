52b7edad77b376b82c8076bdd5af2684e4a494f2
CzlowiekImadlo <quak@tlen.pl>
2016-02-26 21:03:38 +0100

    Reverted default resolutions

--- scripts/services/resolution.gd
+++ scripts/services/resolution.gd
@@ -45,11 +45,11 @@ func apply_resolution():
     var fullscreen = false
 
     if not self.override_resolution:
-        var width = Globals.get('display/width')
-        var height = Globals.get('display/height')
-        self.bag.hud_dead_zone.screen_size = Vector2(width, height)
-        self.bag.workshop_dead_zone.screen_size = Vector2(width, height)
-        OS.set_window_resizable(true)
+        #var width = Globals.get('display/width')
+        #var height = Globals.get('display/height')
+        #self.bag.hud_dead_zone.screen_size = Vector2(width, height)
+        #self.bag.workshop_dead_zone.screen_size = Vector2(width, height)
+        #OS.set_window_resizable(true)
         return
 
     if self.root.settings['resolution'] == self.UNLOCKED:
