a85b89ebc4a93bdeb8c65c5e87c25c064ba6bdc1
CzlowiekImadlo <quak@tlen.pl>
2016-03-10 21:30:19 +0100

    Some english fixes for tips

--- scripts/translations/tips.gd
+++ scripts/translations/tips.gd
@@ -1,16 +1,16 @@
 var tips = []
 
 func _init():
-    self.tips.append('Each unit can attack only once a turn')
+    self.tips.append('Each unit can attack only once per turn')
     self.tips.append('Units have own weaknesses and advantages - only troops can capture buildings but they have almost no chance against tanks and tanks can’t attack helis')
     self.tips.append('You can deploy soldiers in HQ but it is cheaper to do so in the barracks')
-    self.tips.append('Unit losses all his remaining AP in turn when capturing Tower')
+    self.tips.append('Unit losses all of its remaining AP when capturing Tower')
     self.tips.append('All production buildings require a soldier to maintain it - capturing them will cost you a soldier unit')
-    self.tips.append('HQ and captured Towers or Airports will produce cash in every turn')
+    self.tips.append('HQ, Towers and Airports will produce cash in every turn')
     self.tips.append('Cash can be spend for deploying units and movement')
     self.tips.append('Use your cash wisely - a big army will be slower and less effective than a small squad')
-    self.tips.append('It’s never a good idea to send a unit alone to the battlefield')
+    self.tips.append('It’s never a good idea to send a unit alone on the battlefield')
     self.tips.append('Mixing unit types in squads gives you a strategic advantage')
-    self.tips.append('You cannot deploy unit when spawning point is blocked and so do the enemy - block factories with your units')
-    self.tips.append('Units can attack only adjacent tiles - so when target is far away you should firstly move your unit')
-    self.tips.append('HQ is the most valuable building in game - if you capture enemy HQ you will win the game, losing it means instant defeat')
+    self.tips.append('You cannot deploy unit when spawning point is blocked and so does the enemy - block factories with your units')
+    self.tips.append('Units can attack only adjacent tiles - when target is far away you have to move your unit first')
+    self.tips.append('HQ is the most important building in game - if you capture the enemy HQ you will win the game, losing yours means instant defeat')
