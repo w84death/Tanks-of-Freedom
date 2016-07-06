6b93f5bd50008178c5412a82c7b8fe53a8d8e141
Krzysztof Jankowski <w84death@gmail.com>
2016-07-06 17:56:49 +0200

    menu.tscn

--- scripts/controllers/controllers.gd
+++ scripts/controllers/controllers.gd
@@ -1,6 +1,6 @@
 
 var action_controller = preload("res://scripts/controllers/action_controller.gd").new()
-var menu_controller = preload('res://gui/menu.xscn').instance()
+var menu_controller = preload('res://gui/menu.tscn').instance()
 var campaign_menu_controller = preload("res://scripts/controllers/campaign_menu_controller.gd").new()
 var hud_panel_controller = preload("res://scripts/controllers/hud_panel_controller.gd").new()
 var workshop_gui_controller = null #preload("res://scripts/controllers/workshop_gui_controller.gd").new()
