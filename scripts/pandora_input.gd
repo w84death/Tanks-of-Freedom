b6d98832c7b1ba1a8ebea4dc2e927873e943119a
czlowiekimadlo <quak@tlen.pl>
2016-08-08 13:21:19 +0200

    Changed pandora controls to be input event based

--- scripts/pandora_input.gd
+++ scripts/pandora_input.gd
@@ -14,24 +14,24 @@ func handle_input(event):
 
 func handle_button(event):
     if self.bag.workshop.is_working:
-        if event.scancode == KEY_HOME:
+        if Input.is_action_pressed('gamepad_build'):
             if event.pressed:
                 self.bag.root.sound_controller.play('menu')
                 self.bag.workshop.paint(self.bag.workshop.selector_position)
             self.painting = event.pressed
-        if event.scancode == KEY_PAGEUP:
+        if Input.is_action_pressed('gamepad_end'):
             if event.pressed:
                 self.bag.root.sound_controller.play('menu')
                 self.bag.workshop.paint(self.bag.workshop.selector_position, 'terrain', -1)
             self.erasing = event.pressed
     else:
-        if event.scancode == KEY_PAGEUP and event.pressed and not self.bag.root.hud_controller.hud_message_card_visible:
+        if Input.is_action_pressed('gamepad_end') and event.pressed and not self.bag.root.hud_controller.hud_message_card_visible:
             self.bag.root.sound_controller.play('menu')
             self.bag.root.action_controller.end_turn()
-        if event.scancode == KEY_HOME and event.pressed:
+        if Input.is_action_pressed('gamepad_build') and event.pressed:
             self.bag.root.sound_controller.play('menu')
             self.bag.root.action_controller.spawn_unit_from_active_building()
-        if event.scancode == KEY_SHIFT and event.pressed:
+        if Input.is_action_pressed('gamepad_prev') and event.pressed:
             self.bag.root.action_controller.switch_unit(self.bag.unit_switcher.BACK)
-        if event.scancode == KEY_CONTROL and event.pressed:
+        if Input.is_action_pressed('gamepad_next') and event.pressed:
             self.bag.root.action_controller.switch_unit(self.bag.unit_switcher.NEXT)
