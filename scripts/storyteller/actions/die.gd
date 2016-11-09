6466ea588b5a8de6581f87256abe6fc3aa80cb28
CzlowiekImadlo <quak@tlen.pl>
2016-11-09 17:14:59 +0100

    Added collateral damage to storyteller die

--- scripts/storyteller/actions/die.gd
+++ scripts/storyteller/actions/die.gd
@@ -4,3 +4,4 @@ func perform(action_details):
     var field = self.bag.abstract_map.get_field(action_details['who'])
     field.object.die_after_explosion(null)
     field.object = null
+    self.bag.root.action_controller.collateral_damage(action_details['who'])
