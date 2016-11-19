887a76c2081b66f4a2ed03e2bc97c5f8b1e8f93c
CzlowiekImadlo <quak@tlen.pl>
2016-11-19 21:07:42 +0100

    Fixed crash when many threads tried to use same http client

--- scripts/online/request_async.gd
+++ scripts/online/request_async.gd
@@ -1,6 +1,7 @@
 extends "res://scripts/bag_aware.gd"
 
-var thread = Thread.new()
+var threads = []
+var request_template = preload('res://scripts/online/request.gd')
 
 func get(api, resource, calling_object = null, callbacks = {}, expect_json = true):
     self._start_request_thread("GET", api, resource, "", calling_object,callbacks, expect_json)
@@ -9,8 +10,7 @@ func post(api, resource, data = "", calling_object = null, callbacks = {}, expec
     self._start_request_thread("POST", api, resource, data, calling_object, callbacks, expect_json)
 
 func _start_request_thread(method, api, resource, data = "", calling_object = null, callbacks = {}, expect_json = true):
-    if self.thread.is_active():
-        return
+    var thread = self._get_free_thread()
 
     var request_params = {
         'method' : method,
@@ -19,10 +19,11 @@ func _start_request_thread(method, api, resource, data = "", calling_object = nu
         'data' : data,
         'calling_object' : calling_object,
         'callbacks' : callbacks,
-        'expect_json' : expect_json
+        'expect_json' : expect_json,
+        'thread' : thread,
     }
 
-    self.thread.start(self, '_make_request', request_params)
+    thread.start(self, '_make_request', request_params)
 
 func _make_request(parameters):
     var method = parameters['method']
@@ -32,10 +33,13 @@ func _make_request(parameters):
     var expect_json = parameters['expect_json']
     var response = {}
 
+    var request = self.request_template.new()
+    request._init_bag(self.bag)
+
     if method == "GET":
-        response = self.bag.online_request.get(api, resource, expect_json)
+        response = request.get(api, resource, expect_json)
     elif method == "POST":
-        response = self.bag.online_request.post(api, resource, data, expect_json)
+        response = request.post(api, resource, data, expect_json)
     else:
         response = {
             'status' : "error",
@@ -46,12 +50,12 @@ func _make_request(parameters):
 
     parameters['response'] = response
 
-    call_deferred("_execute_callback")
+    call_deferred("_execute_callback", parameters['thread'])
 
     return parameters
 
-func _execute_callback():
-    var parameters = self.thread.wait_to_finish()
+func _execute_callback(thread):
+    var parameters = thread.wait_to_finish()
     var calling_object = parameters['calling_object']
     var callbacks = parameters['callbacks']
     var response = parameters['response']
@@ -61,3 +65,14 @@ func _execute_callback():
             calling_object.call(callbacks["handle_" + str(response['response_code'])], response)
         else:
             calling_object.call(callbacks["handle_error"], response)
+
+
+func _get_free_thread():
+    for thread in self.threads:
+        if not thread.is_active():
+            return thread
+
+    var new_thread = Thread.new()
+    self.threads.append(new_thread)
+
+    return new_thread
