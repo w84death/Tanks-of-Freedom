798fbfd193ce67dc58a50e51732d75c04c5319ef
CzlowiekImadlo <quak@tlen.pl>
2018-01-07 15:16:03 +0100

    Fixed crash when using Die or Despawn story actions on empty fields

--- scripts/storyteller/actions/despawn.gd
+++ scripts/storyteller/actions/despawn.gd
@@ -2,6 +2,10 @@ extends "res://scripts/storyteller/actions/abstract_action.gd"
 
 func perform(action_details):
     var field = self.bag.abstract_map.get_field(action_details['who'])
+
+    if field.object == null:
+        return
+
     field.object.queue_free()
     field.object = null
 
--- scripts/storyteller/actions/die.gd
+++ scripts/storyteller/actions/die.gd
@@ -2,6 +2,10 @@ extends "res://scripts/storyteller/actions/abstract_action.gd"
 
 func perform(action_details):
     var field = self.bag.abstract_map.get_field(action_details['who'])
+
+    if field.object == null:
+        return
+
     field.object.die_after_explosion(null)
     self.bag.root.sound_controller.play_unit_sound(field.object, 'die')
     field.object = null
