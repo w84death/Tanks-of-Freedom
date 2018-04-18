2c7752a4da1544fd6e91cf645dae9a0c353fa0cb
jasam <p.weremczuk@gmail.com>
2018-04-18 16:53:27 +0200

    add check before move action

--- scripts/ai/actions/handlers/void.gd
+++ scripts/ai/actions/handlers/void.gd
@@ -4,7 +4,6 @@ func _init(bag):
     self.bag = bag
 
 func execute(action):
-    print(' void action ')
     self.bag.actions_handler.remove(action)
     if self.get_actions_for_unit(action.unit).size() == 0:
         self.mark_unit_for_calculations(action.unit)
--- scripts/yuri/actions/hands/move_hand.gd
+++ scripts/yuri/actions/hands/move_hand.gd
@@ -3,9 +3,12 @@ extends "res://scripts/yuri/actions/hands/base_hand.gd"
 func _initialize():
     self.handled_action = preload("res://scripts/yuri/actions/types/move_action.gd")
 
-
 func execute(action):
     if action.path.size() < 2:
+        return
+
+    var field = self.bag.abstract_map.get_field(action.path[1])
+    if field.object != null && field.object.player == self.bag.controllers.action_controller.current_player:
         return
 
     self.bag.controllers.action_controller.set_active_field(action.entity.position_on_map)
