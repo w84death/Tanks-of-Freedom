85525af8a0b952ac70a1e32572eede1e4caf4ac4
CzlowiekImadlo <quak@tlen.pl>
2019-01-10 15:18:29 +0100

    Integrated all new language PRs

--- translations/languages.gd
+++ translations/languages.gd
@@ -1,5 +1,5 @@
 # only confirmed and finished translations here - they will be use on build
-var available = ['en', 'de', 'fr', 'ja', 'pl', 'ru', 'es']
+var available = ['en', 'de', 'es', 'fr', 'ja', 'pl', 'ru']
 
 # languages available locally - ADD OWN LANG HERE
 var in_develop = []
